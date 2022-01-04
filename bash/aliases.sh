6504d2048b5b123aed6831e109775a2440c8d376
Guillaume Latour <guillaume.latour@macq.eu>
2022-01-04 11:16:04 +0100

    fucking CLRF

--- bash/aliases.sh
+++ bash/aliases.sh
@@ -1,17 +1,22 @@
+# Custom programs
 alias playground="/opt/scripts/playground.sh"
 alias pdfmerge="/opt/scripts/pdfmerger/pdfmerger.sh"
 
+# Editors
 alias vim=nvim
 alias vi=nvim
 alias v=nvim
 
+# ls
 alias l="ls -lAh"
 alias la="ls -A"
 alias ll="ls -l"
 
+# Git
 alias g="git"
 alias gcm="git commit -m"
 alias ga="git add"
 alias gst="git status"
 
+# Grep
 alias grep="grep --color"
