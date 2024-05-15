1b453add6602d6a6a255904ed8c3ca2c88dc3c9e
Guillaume Latour <guillaume.latour@macq.eu>
2024-05-15 15:54:02 +0200

    clean alacritty stuff

--- bash/ssh.sh
+++ bash/ssh.sh
@@ -19,7 +19,7 @@ if [ -f "${SSH_ENV}" ]; then
     . "${SSH_ENV}" > /dev/null
     #ps ${SSH_AGENT_PID} doesn't work under cywgin
     ps -ef | grep ${SSH_AGENT_PID} | grep ssh-agent$ > /dev/null || {
-        start_agent;
+        reload_agent;
     }
 else
     start_agent;
